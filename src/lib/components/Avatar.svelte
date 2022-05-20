373fe58c816107d875bee0c43bdf8475c8d85cf9
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-05-21 00:07:36 +0430

    feat: Avatar component built

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,22 @@
+<script>
+    import configuration from "$lib/config";
+
+    let avatarImage = configuration.assets.image;
+    let secondaryColor = configuration.colors.secondary;
+</script>
+
+<img 
+    src={avatarImage}
+    style={`color: ${secondaryColor}`}
+    alt="avatar"
+    width="180"
+    height="180"
+/>
+
+<style>
+    img {
+        border: 2px solid currentColor;
+        border-radius: 50%;
+        margin: 3% 0% 4% 5%;
+    }
+</style>
